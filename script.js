42dad3bd989947434e5d39906b7f78635bf6a8aa
Annaliza_C <aldcadiente@gmail.com>
2024-02-01 17:18:07 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+var name = prompt("what is your name?");
+var birthYear = prompt("what year were you born? (ex. 1982)");
+
+console.log(`Your name is ${name} and you're ${2024 - birthYear} years old.`);
